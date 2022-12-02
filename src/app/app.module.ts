3ebe94fbd7e1a016f7b5a0ad321563e9ca2511df
davidkhachatryan123 <xdavit777@icloud.com>
2022-12-03 00:53:06 +0400

    00:52 03-Dec-22

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { LoadingComponent } from './website/loading/component/loading.component';
 
 import { WebSiteModule } from './website/website.module';
 
+import { LoadingInterceptor } from './website/loading/interceptor/loading.interceptor';
+
 @NgModule({
-  imports: [ BrowserModule, RouterModule.forRoot([]),
+  imports: [ BrowserModule, HttpClientModule, RouterModule.forRoot([]),
              WebSiteModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, LoadingComponent ],
   bootstrap: [ AppComponent ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true
+    }
+  ]
 })
 export class AppModule { }
