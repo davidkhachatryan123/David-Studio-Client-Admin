1f13f2b84ead54679841c87780462a651b812d03
davidkhachatryan123 <xdavit777@icloud.com>
2022-12-06 21:54:30 +0400

    21:54 06-Dec-22

--- src/app/website/routing/dashboard/pages/home/home.module.ts
+++ src/app/website/routing/dashboard/pages/home/home.module.ts
@@ -10,6 +10,7 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 import { ServicesComponent } from './containers';
 import { ServiceFormComponent, ServicesTabsComponent, ServiceLanguageComponent } from './componenets';
@@ -25,6 +26,7 @@ import { ServiceFormComponent, ServicesTabsComponent, ServiceLanguageComponent }
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
+    MatAutocompleteModule,
   ],
   declarations: [
     ServicesComponent,
