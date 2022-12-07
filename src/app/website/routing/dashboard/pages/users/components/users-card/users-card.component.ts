2c53b02032c1fef3d75513a13438af9bddff0c78
davidkhachatryan123 <xdavit777@icloud.com>
2022-12-07 23:43:36 +0400

    23:43 07-Dec-22

--- src/app/website/routing/dashboard/pages/users/components/users-card/users-card.component.ts
+++ src/app/website/routing/dashboard/pages/users/components/users-card/users-card.component.ts
@@ -1,18 +1,21 @@
-import { Component, AfterViewInit, ViewChild, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, Input, Output, EventEmitter } from '@angular/core';
 
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTable } from '@angular/material/table';
 
 import { TableOptions } from 'src/app/website/models';
+import { AppUser } from 'src/app/website/routing/auth/models';
 import { NewUser, User } from 'src/app/website/routing/dashboard/pages/users/models';
+
+import { AuthService } from 'src/app/website/routing/auth/services';
 
 @Component({
   selector: 'app-dashboard-users-card',
   templateUrl: 'users-card.component.html',
   styleUrls: [ 'users-card.component.css' ]
 })
-export class UsersCardComponent implements AfterViewInit {
+export class UsersCardComponent implements OnInit, AfterViewInit {
   displayedColumns: string[] = ['username', 'email', 'emailConfirmed', 'phone', 'role', 'actions'];
 
   @Input() data: User[] = [];
@@ -28,6 +31,16 @@ export class UsersCardComponent implements AfterViewInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   private userListOptions: TableOptions = new TableOptions('', '', 0, 0);
+
+  appUser: AppUser = new AppUser('', '', '', '');
+
+  constructor(
+    private authService: AuthService
+  ) { }
+
+  ngOnInit() {
+    this.authService.getUser().subscribe(data => this.appUser = data);
+  }
 
   ngAfterViewInit() {
     this.onChangeEvent();
